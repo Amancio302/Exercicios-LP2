2950221fe65960767a1804dac9f64c3245f65083
amancio302 <matheusamancioferreira@gmail.com>
2017-09-27 09:50:53 -0300

    Desafio concluido

--- CRUDSQLiteII/app/src/main/java/com/example/alunos/crudsqliteii/AlteraActivity.java
+++ CRUDSQLiteII/app/src/main/java/com/example/alunos/crudsqliteii/AlteraActivity.java
@@ -1,5 +1,7 @@
 package com.example.alunos.crudsqliteii;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +9,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AlteraActivity extends AppCompatActivity {
 
@@ -49,10 +52,25 @@ public class AlteraActivity extends AppCompatActivity {
         deletar.setOnClickListener( new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                crud.apagaRegistro(Integer.parseInt(codigo));
-                Intent intent = new Intent(AlteraActivity.this, ConsultaActivity.class);
-                startActivity(intent);
-                finish();
+                AlertDialog.Builder builder = new AlertDialog.Builder(AlteraActivity.this);
+                builder.setTitle("Tem certeza?");
+                builder.setMessage("Tem certeza?");
+                builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(AlteraActivity.this, "Registro não Excluido", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface arg0, int arg1) {
+                        Toast.makeText(AlteraActivity.this, "Registro Excluido", Toast.LENGTH_SHORT).show();
+                        crud.apagaRegistro(Integer.parseInt(codigo));
+                        Intent intent = new Intent(AlteraActivity.this, ConsultaActivity.class);
+                        startActivity(intent);
+                        finish();
+                    }
+                });
+                AlertDialog alerta = builder.create();
+                alerta.show();
             }
         });
     }
